b2de6b13b1aac2f629fc6f5b98b510ea042c2f12
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-24 17:33:41 -0600

    More variables

--- RandomLittleThings/variables.py
+++ RandomLittleThings/variables.py
@@ -8,3 +8,11 @@ sales_tax = .08 * sandwich_price
 
 sandwich_price += sales_tax
 money_in_wallet -= sandwich_price
+print money_in_wallet
+
+cucumbers = 1
+price_per_cucumber = 3.25
+
+total_cost = cucumbers * price_per_cucumber
+
+print total_cost
