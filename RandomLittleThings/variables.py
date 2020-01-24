9ec94ab6adbde8556e21f93ed5a6cc182ac8c806
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-24 17:59:57 -0600

    Finished Learning Basic Variable Syntax

--- RandomLittleThings/variables.py
+++ RandomLittleThings/variables.py
@@ -40,4 +40,4 @@ points_per_exercise = 5
 point_total = 100
 point_total += exercises_completed * points_per_exercise
 
-print "I got " + str(point_total) + " points!"
+print "I got " + str(point_total) + " points!" 
