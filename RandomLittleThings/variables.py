ac5a9ecfd2fd6879e04ae423177fd26852d2c69c
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-24 17:39:37 -0600

    More variables

--- RandomLittleThings/variables.py
+++ RandomLittleThings/variables.py
@@ -16,3 +16,11 @@ price_per_cucumber = 3.25
 total_cost = cucumbers * price_per_cucumber
 
 print total_cost
+
+cucumbers = 100
+num_people = 6
+
+whole_cucumbers_per_person = cucumbers/num_people
+float_cucumbers_per_person = float(cucumbers)/num_people
+print (whole_cucumbers_per_person)
+print (float_cucumbers_per_person)
