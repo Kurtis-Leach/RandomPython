66f014403d41a059445f068dd9e95ad28b64409d
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-24 17:53:38 -0600

    Finished Learning Basic Variable Syntax

--- RandomLittleThings/variables.py
+++ RandomLittleThings/variables.py
@@ -17,6 +17,7 @@ total_cost = cucumbers * price_per_cucumber
 
 print total_cost
 
+#when only integers then it rounds down must be float to get a float
 cucumbers = 100
 num_people = 6
 
@@ -24,3 +25,19 @@ whole_cucumbers_per_person = cucumbers/num_people
 float_cucumbers_per_person = float(cucumbers)/num_people
 print (whole_cucumbers_per_person)
 print (float_cucumbers_per_person)
+
+#multiline strings
+haiku = """The old pond,
+A frog jumps in:
+Plop!"""
+
+#True and False are capatilized
+
+skill_completed = "Python Syntax"
+exercises_completed = 13
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise = 5
+point_total = 100
+point_total += exercises_completed * points_per_exercise
+
+print "I got " + str(point_total) + " points!"
