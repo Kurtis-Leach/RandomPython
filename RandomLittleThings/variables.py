1af3e51df21b72be3cfe424bafff2eba204815b1
BobNarwhalKing <danceflurry4366@gmail.com>
2020-01-24 16:28:43 -0600

    Changing Variables

--- RandomLittleThings/variables.py
+++ RandomLittleThings/variables.py
@@ -1,3 +1,10 @@
 variableName = 'What your variable is'
 
 print variableName
+
+money_in_wallet = 40
+sandwich_price = 7.50
+sales_tax = .08 * sandwich_price
+
+sandwich_price += sales_tax
+money_in_wallet -= sandwich_price
